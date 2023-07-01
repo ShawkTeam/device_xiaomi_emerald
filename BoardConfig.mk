7c42411372ff420063fe61538b2a7b4cd0afff49
Christian Oder <myself5@carbonrom.org>
2023-07-01 17:20:17 +0700

    emerald: Build disabled VBMeta image
    Change-Id: I48e3ff106a2e4e044ed39b86a94f3bc80b93f530
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,5 +63,9 @@ TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+
 # Inherit the proprietary files
 include vendor/xiaomi/emerald/BoardConfigVendor.mk
