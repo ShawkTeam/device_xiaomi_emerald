2057bc7aafaac7a36bc7d6d113d7ac3a81f574a8
bengris32 <bengris32@protonmail.ch>
2023-07-07 22:56:23 +0700

    emerald: Enable support for EXT4 & F2FS formatted userdata
    Change-Id: I88ce22a250e1f7e5882cb225cca8691af587985a
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,6 +93,8 @@ BOARD_HAS_MTK_HARDWARE := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.mt6789
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
