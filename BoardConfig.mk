8ebb74fb382727076c143ee5e5974d17344f3b4a
bengris32 <bengris32@protonmail.ch>
2023-07-07 23:06:59 +0700

    emerald: Configure Virtual A/B OTAs
    * Enable recovery-as-boot for A/B device.
    * Build updater packages for A/B device.
    * Include checkpoint_gc & otapreopt_script postinstall
      scripts.
    * Configure partitions which will be included in OTA package.
    * Explicitly disable BOARD_BUILD_SYSTEM_ROOT_IMAGE on A/B device
      using 2-stage-init.
    * Inherit the virtual_ab_ota.mk and compression_retrofit.mk
      product configurations as seen on stock.
    
    Change-Id: Ia17a18c1d47b9d872073238223e7c606cc47283c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,20 @@
 
 DEVICE_PATH := device/xiaomi/emerald
 
+# A/B
+AB_OTA_UPDATER := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    product \
+    vendor \
+    vbmeta \
+    vendor_boot
+
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
