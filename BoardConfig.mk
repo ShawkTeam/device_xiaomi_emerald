8912b2bb81e71c1a146ac4671099c0167794315a
Arian <arian.kulmer@web.de>
2023-07-25 21:23:35 +0100

    emerald: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,9 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_BOARD_PLATFORM := mt6789
 BOARD_HAS_MTK_HARDWARE := true
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/props/vendor.prop
 
