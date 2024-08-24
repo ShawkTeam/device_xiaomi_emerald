3b5d8e8cb07175bdea680a376d27d4df25c3bc2a
Allien10X <erdemiryz38@gmail.com>
2024-08-24 23:29:00 +0300

    emerald: Initial minimised rootdir
    Signed-off-by: Allien10X <erdemiryz38@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    fstab.mt6789 \
+    fstab.emmc \
+    fstab.enableswap \
+    init.connectivity.common.rc \
+    init.cgroup.rc \
+    init_connectivity.rc \
+    init.modem.rc \
+    init.mt6789.rc \
+    init.mt6789.usb.rc \
+    init.mtkgki.rc \
+    init.project.rc \
+    init.sensor_2_0.rc \
+    init.recovery.mt6789.rc \
+    init.insmod.sh \
+    ueventd.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
